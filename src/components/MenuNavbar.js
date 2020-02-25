0180dae4f38cb0f326908032ddec318faeebba03
Jason Webb <jwebb@accessible360.com>
2020-02-25 16:49:22 -0600

    #96 Wrapping main nav in <nav> + list, replacing inner buttons with Icon components, giving icons meaningful titles, adding styling to match current look

--- src/components/MenuNavbar.js
+++ src/components/MenuNavbar.js
@@ -1,40 +1,79 @@
 import React from 'react';
-import { Button, Position, Tooltip } from '@blueprintjs/core';
+import { Icon, Position, Tooltip } from '@blueprintjs/core';
 
 const NavbarMenu = () => {
   return (
-    <div>
-      <Tooltip content="Home" position={Position.BOTTOM}>
-        <a href="#home">
-          <Button className="pt-minimal pt-large white" icon="home" />
-        </a>
-      </Tooltip>
-      <Tooltip content="About" position={Position.BOTTOM}>
-        <a href="#about">
-          <Button className="pt-minimal pt-large white" icon="info-sign" />
-        </a>
-      </Tooltip>
-      <Tooltip content="Events" position={Position.BOTTOM}>
-        <a href="#events">
-          <Button className="pt-minimal pt-large white" icon="calendar" />
-        </a>
-      </Tooltip>
-      <Tooltip content="Community" position={Position.BOTTOM}>
-        <a href="#community">
-          <Button className="pt-minimal pt-large white" icon="people" />
-        </a>
-      </Tooltip>
-      <Tooltip content="Code of Conduct" position={Position.BOTTOM}>
-        <a href="#code-of-conduct">
-          <Button className="pt-minimal pt-large white" icon="take-action" />
-        </a>
-      </Tooltip>
-      <Tooltip content="Contact" position={Position.BOTTOM}>
-        <a href="#contact">
-          <Button className="pt-minimal pt-large white" icon="envelope" />
-        </a>
-      </Tooltip>
-    </div>
+    <nav aria-label="main">
+      <ul className="pt-list-unstyled">
+        <li>
+          <Tooltip content="Home" position={Position.BOTTOM}>
+            <a href="#home">
+              <Icon icon="home" iconSize="16" color="white" title="home" />
+            </a>
+          </Tooltip>
+        </li>
+        <li>
+          <Tooltip content="About" position={Position.BOTTOM}>
+            <a href="#about">
+              <Icon
+                icon="info-sign"
+                iconSize="16"
+                color="white"
+                title="about"
+              />
+            </a>
+          </Tooltip>
+        </li>
+        <li>
+          <Tooltip content="Events" position={Position.BOTTOM}>
+            <a href="#events">
+              <Icon
+                icon="calendar"
+                iconSize="16"
+                color="white"
+                title="events"
+              />
+            </a>
+          </Tooltip>
+        </li>
+        <li>
+          <Tooltip content="Community" position={Position.BOTTOM}>
+            <a href="#community">
+              <Icon
+                icon="people"
+                iconSize="16"
+                color="white"
+                title="community"
+              />
+            </a>
+          </Tooltip>
+        </li>
+        <li>
+          <Tooltip content="Code of Conduct" position={Position.BOTTOM}>
+            <a href="#code-of-conduct">
+              <Icon
+                icon="take-action"
+                iconSize="16"
+                color="white"
+                title="code of conduct"
+              />
+            </a>
+          </Tooltip>
+        </li>
+        <li>
+          <Tooltip content="Contact" position={Position.BOTTOM}>
+            <a href="#contact">
+              <Icon
+                icon="envelope"
+                iconSize="16"
+                color="white"
+                title="contact"
+              />
+            </a>
+          </Tooltip>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
